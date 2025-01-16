3ba6be153af5881f3222c27f6ef7afb72c0d38ff
boyanfan <fanboyan@iclooud.com>
2025-01-16 18:16:27 -0500

    Update: Start implpementing tokens.

---
+++ opus-compiler/main.c
@@ -0,0 +1,9 @@
+// main.c
+//
+// Created by fanboyan on 2025/1/16.
+//
+
+int main() {
+    // ...
+    return 0;
+}
--- opus-compiler/opus-lexer/includes/token.h
+++ opus-compiler/opus-lexer/includes/token.h
@@ -12,4 +12,38 @@
 #ifndef TOKEN_H
 #define TOKEN_H
 
-#endif
+/// Token types that need to be recognized by the lexer.
+typedef enum {
+    TOKEN_EOF,                   // The end of the input file or stream
+    TOKEN_NUMBER,                // Numeric values like "42", ...
+    TOKEN_ARITHMETIC_OPERATOR,   // Arithmetic operators like "+", "-", "*" and "/", ...
+    TOKEN_ERROR                  // Invalid or unrecognized token
+} TokenType;
+
+/// Error types for lexical analysis
+typedef enum {
+    ERROR_NONE,             // No error occurred
+    ERROR_INVALID_CHAR,     // An invalid or unrecognized character was encountered
+    ERROR_INVALID_NUMBER,   // A number was malformed or invalid
+} TokenError;
+
+/// The location of a token in the source code.
+typedef struct {
+    /// The line number where the token begins and starting column of the token (both starting from 1).
+    int line, column;
+} Location;
+
+/// Token structure to store token information in the lexical analysis process.
+/// It tracks the line number and the start column of the token.
+typedef struct {
+    /// The type of the token (e.g. TOKEN_NUMBER, TOKEN_EOF).
+    TokenType tokenType;
+    /// Any error associated with the token (e.g., ERROR_INVALID_CHAR).
+    TokenError tokenError;
+    /// The location of the token in the source code (line and column).
+    Location location;
+    /// The actual content (lexeme) of the token
+    char lexeme[128];
+} Token;
+
+#endif /* TOKEN_H */
